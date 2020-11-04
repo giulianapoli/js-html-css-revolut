190b8f2f35b8936ae6cf6805d1198f71010dd25d
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-04 19:37:13 +0100

    Modifies mouseenter/leave functions

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
   var optionList = $('ul li > a');
   var dropdownMenu = $('.dropdownContent');
-  var active = $('.dropdownContent.active')
+  var active = $('.dropdownContent.active');
+  var menuEl = $('.dropdownContent > .menu');
 
   // console.log(dropdownMenu);
   // console.log(optionList);
 
+  $('.dropdownContent').removeClass('active');
+  dropdownMenu.removeClass('active');
 
-
-  optionList.click(function (){
-
-    $('.dropdownContent').removeClass('active');
+  optionList.mouseenter(function (){
     $(this).next('.dropdownContent').addClass('active');
+    $(this).next('optionList').addClass('active');
   });
+
+  optionList.mouseleave(function (){
+    $('.dropdownContent').removeClass('active');
+  });
+
+  // menuEl.mouseenter(function () {
+  //   $(this).children('.dropdownContent').addClass('active');
+  // });
+  //
+  // menuEl.mouseleave(function () {
+  //   $(this).children('.dropdownContent').removeClass('active');
+  // });
 
 });
