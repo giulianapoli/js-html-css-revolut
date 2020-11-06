d53336fe3e04dbe758e29bad1cd8c2e5f00fa920
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-06 16:28:37 +0100

    Corrects .click bugs, complete!

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
-  var optionList = $('ul li > a');
-  var dropdownMenu = $('.dropdownContent');
+  var liEl = $('.has-dropdown');
   var active = $('.dropdownContent.active');
-  var menuEl = $('.dropdownContent > .menu');
 
-  // console.log(dropdownMenu);
-  // console.log(optionList);
 
-  $('.dropdownContent').removeClass('active');
-  dropdownMenu.removeClass('active');
+  liEl.click(function(){
+    var dropdownMenu = $('.dropdownContent');
 
-  optionList.mouseenter(function (){
-    $(this).next('.dropdownContent').addClass('active');
-    $(this).next('optionList').addClass('active');
-  });
-
-  optionList.mouseleave(function (){
-    $('.dropdownContent').removeClass('active');
-  });
-
-  // menuEl.mouseenter(function () {
-  //   $(this).children('.dropdownContent').addClass('active');
-  // });
-  //
-  // menuEl.mouseleave(function () {
-  //   $(this).children('.dropdownContent').removeClass('active');
-  // });
-
+    if ($(this).children('.dropdownContent').hasClass('active')) {
+      $(this).children('.dropdownContent').removeClass('active');
+    } else {
+      dropdownMenu.removeClass('active');
+      $(this).children('.dropdownContent').addClass('active');
+    }
+  })
 });
